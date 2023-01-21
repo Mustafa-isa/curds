6d0a770de8166ca27705d38da19b03bc59faf2d0
Mustafa-isa <116859259+Mustafa-isa@users.noreply.github.com>
2023-01-21 10:53:56 +0200

    cruds
    create a function of total

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
 //SATRTING CODE............
+const title = document.getElementById("title");
+const price = document.getElementById("price");
+const taxes = document.getElementById("tax");
+const ads = document.getElementById("ads");
+const discount = document.getElementById("dis");
+const total = document.querySelector(".totle");
+
+const count = document.getElementById("count");
+const catogrey = document.getElementById("catogry");
+const search = document.getElementById("search");
+
+function totalPrice() {
+  let result;
+  if (price.value != "") {
+    result = +price.value + +taxes.value +  +ads.value  -discount.value;
+    total.innerHTML= result;
+    total.style.background = '#040'
+  }else{
+    total.style.background = 'red'
+    
+  }
+}
+
+
+//get total of product
