03e77ab8c67856b47a25c1b0b029b825531d366f
Mustafa-isa <116859259+Mustafa-isa@users.noreply.github.com>
2023-01-21 11:44:35 +0200

    cruds
    change something

--- main.js
+++ main.js
@@ -5,22 +5,20 @@ const taxes = document.getElementById("tax");
 const ads = document.getElementById("ads");
 const discount = document.getElementById("dis");
 const total = document.querySelector(".totle");
-
 const count = document.getElementById("count");
 const catogrey = document.getElementById("catogry");
 const search = document.getElementById("search");
+const color= document.getElementById('color')
 
 function totalPrice() {
   let result;
   if (price.value != "") {
-    result = +price.value + +taxes.value +  +ads.value  -discount.value;
-    total.innerHTML= result;
-    total.style.background = '#040'
-  }else{
-    total.style.background = 'red'
-    
+    result = +price.value + +taxes.value + +ads.value - discount.value;
+    total.innerHTML = result;
+    total.style.background = "#040";
+  } else {
+    total.style.background = "red";
   }
 }
-
-
+ 
 //get total of product
